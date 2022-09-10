546cb1f90319c5189a3a2b47d55ed7090a45159c
Marcel Malych <marcelmalych5@gmail.com>
2022-09-10 13:59:37 +0200

    lualine responsivity

--- .config/nvim/lua/config/lualine.lua
+++ .config/nvim/lua/config/lualine.lua
@@ -1,6 +1,39 @@
-require('lualine').setup {
-    options= {
-        icons_enabled = true,
-        theme = "gruvbox-material"
-    }
-}
+--- @param trunc_width number trunctates component when screen width is less then trunc_width
+--- @param trunc_len number truncates component to trunc_len number of chars
+--- @param hide_width number hides component when window width is smaller then hide_width
+--- @param no_ellipsis boolean whether to disable adding '...' at end after truncation
+--- return function that can format the component accordingly
+local function trunc(trunc_width, trunc_len, hide_width, no_ellipsis)
+	return function(str)
+		local win_width = vim.fn.winwidth(0)
+		if hide_width and win_width < hide_width then
+			return ""
+		elseif trunc_width and trunc_len and win_width < trunc_width and #str > trunc_len then
+			return str:sub(1, trunc_len) .. (no_ellipsis and "" or "...")
+		end
+		return str
+	end
+end
+
+require("lualine").setup({
+	options = {
+		icons_enabled = true,
+		theme = "gruvbox-material",
+	},
+	sections = {
+		lualine_a = { "mode" },
+		lualine_b = { { "branch", fmt = trunc(70, 4, 70, true) }, "diff", "diagnostics" },
+		lualine_c = { "filename" },
+		lualine_x = { "filetype" },
+		lualine_y = { { "progress", fmt = trunc(70, 4, 75, true) } },
+		lualine_z = { "location" },
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = { "filename" },
+		lualine_x = { "filetype", "location" },
+		lualine_y = {},
+		lualine_z = {},
+	},
+})
