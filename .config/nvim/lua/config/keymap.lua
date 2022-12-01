a8a7e49026f2d9ec39d5da6ab981306bca4c567a
Marcel Malych <marcelmalych5@gmail.com>
2022-12-01 10:30:41 +0100

    gitsigns keybinds

--- .config/nvim/lua/config/keymap.lua
+++ .config/nvim/lua/config/keymap.lua
@@ -39,11 +39,17 @@ vim.keymap.set("n", "gr", ":Telescope lsp_references<CR>", Keymap_opts)
 -- fugitive vim.keymap.set
 vim.keymap.set("n", "<Leader>gs", ":G <CR>", Keymap_opts)
 vim.keymap.set("n", "<Leader>gc", ":G commit <CR>", Keymap_opts)
-vim.keymap.set("n", "<leader>gj", ":diffget //3<CR>", Keymap_opts)
-vim.keymap.set("n", "<leader>gf", ":diffget //2<CR>", Keymap_opts)
+vim.keymap.set("n", "<Leader>gj", ":diffget //3<CR>", Keymap_opts)
+vim.keymap.set("n", "<Leader>gf", ":diffget //2<CR>", Keymap_opts)
 
 -- Neoformat
 vim.keymap.set("n", "<Leader>n", ":Neoformat <CR>", Keymap_opts)
 
 -- increase number
 vim.keymap.set("i", "<C-c>", "<Esc>", Keymap_opts)
+
+-- git signs
+vim.keymap.set("n", "<Leader>gts", ":Gitsigns toggle_signs<CR>", Keymap_opts)
+vim.keymap.set("n", "<Leader>gtl", ":Gitsigns toggle_linehl<CR>", Keymap_opts)
+vim.keymap.set("n", "<Leader>gtd", ":Gitsigns toggle_word_diff<CR>", Keymap_opts)
+vim.keymap.set("n", "<Leader>gtb", ":Gitsigns toggle_current_line_blame<CR>", Keymap_opts)
