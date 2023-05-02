d53564e072e576b21e3ba08ce1390f891ba979cc
tremar123 <marcelmalych5@gmail.com>
2023-05-02 12:27:49 +0200

    thanks for broken icons nerd fonts!

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -12,10 +12,10 @@ return {
 			local lspconfig = require("lspconfig")
 
 			local signs = {
-				{ name = "DiagnosticSignError", text = "" },
-				{ name = "DiagnosticSignWarn", text = "" },
-				{ name = "DiagnosticSignHint", text = "" },
-				{ name = "DiagnosticSignInfo", text = "" },
+				{ name = "DiagnosticSignError", text = "" },
+				{ name = "DiagnosticSignWarn", text = "" },
+				{ name = "DiagnosticSignHint", text = "󰌶" },
+				{ name = "DiagnosticSignInfo", text = "" },
 			}
 
 			for _, sign in ipairs(signs) do
